797c61e409fdea21b54a1d91c206f208133605d8
level451 <twitzel@gmail.com>
2019-06-27 08:58:54 -0600

    add multicast binding to all interfaces

--- connector/udpServer.js
+++ connector/udpServer.js
@@ -55,15 +55,15 @@ udpSocket.on('error', (e) => {
 });
 udpSocket.startUdpServer = function () {
     return new Promise(function (resolve, reject) {
-        udpSocket.bind(41235, '10.6.1.2',() => {
+        udpSocket.bind(41235,() => {
             try {
 
                 let ip = getIPv4NetworkInterfaces();
                 console.log(`${ip.length} Network Interfaces found`);
-                udpSocket.addMembership('224.0.0.49'); // dont care what interface right now
+              //  udpSocket.addMembership('224.0.0.49'); // dont care what interface right now
 
                 for (let i = 0; i < ip.length; ++i) {
-                 //   udpSocket.addMembership('224.0.0.49', ip[i].address); // dont care what interface right now
+                    udpSocket.addMembership('224.0.0.49', ip[i].address); // dont care what interface right now
                     console.log(`UDP Multicast Bound to 224.0.0.49 IFace:${ip[i].address}`);
                 }
             } catch (e) {
