eb01470a162b04a54cf2c9d4cdc67e7093224ff3
level451 <twitzel@gmail.com>
2020-01-28 10:35:51 -0700

    added rxonly support in rios (udpServer)

--- httpServer/webSocketServer.js
+++ httpServer/webSocketServer.js
@@ -54,7 +54,7 @@ module.exports.startWebSocketServer = function (server) {
             //console.log('Browser Connected - systemType', ws.systemType,ws.id);
         } else {
             webSocketEmitter.emit('connect', {id: ws.id, systemType: ws.systemType});
-            //  console.log('Rio Connected - systemType & connected', ws.systemType,ws.id,global.settings.connectedRios[ws.id].connected);
+              console.log('Rio Connected - systemType & connected', ws.systemType,ws.id,global.settings.connectedRios[ws.id].connected);
             if (ws.systemType == 'RIO'){
                 settings.connectedRios[ws.id].connected = true;
             }
